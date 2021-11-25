8b52a6ffb04d3cad195c135c825ec0682fa5f2f0
modman484 <74793637+modman484@users.noreply.github.com>
2021-11-25 11:02:18 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,32 @@
-#!/bin/bash
 
-echo "press 1 for readme.txt, press 2 for new game, press 3 to load saved file"
+    BG_CYAN="$(tput setab 6)"
+    BG_BLACK="$(tput setab 0)"
+    FG_GREEN="$(tput setaf 2)"
+    FG_MAGENTA="$(tput setaf 5)"
+function resetVar() {
+    echo "Resetting Lark environment for new game, $userName "
+    sleep 2
+    # this is where you could reset variables for new game play
+    }
 
-     case $selection in
-        1) read /readme.txt
-        
-        ;;
-       esac
-       done
+        tput smcup
+
+    while [[ $REPLY != 0 ]]; do
+      echo -n ${BG_BLACK} ${FG_GREEN}
+      clear
+
+
+# cat uses input of a here document
+      cat <<EOF
+        Please Select:
+        1. Log into Lark game
+        2. readme
+        3. Start Lark game script
+        0. Quit
+EOF
+read -p "Enter selection [0-3] > " selection
+      # Clear area beneath menu
+      tput cup 10 0 # positions the cursor
+      echo -n ${BG_BLACK}${FG_GREEN}
+      tput ed # Clears curosr to end of line
+      tput cup 11 0
