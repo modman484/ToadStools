a07c4fab92b9691950692e44f34931dba1d78c61
modman484 <74793637+modman484@users.noreply.github.com>
2021-11-25 10:47:38 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+echo "press 1 for readme.txt, press 2 for new game, press 3 to load saved file"
+
+     case $selection in
+        1) read /readme.txt
+        
+        ;;
+       esac
+       done
