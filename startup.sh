1b4b32833a3d3ce6fc75273c51ade43d1ef82646
modman484 <74793637+modman484@users.noreply.github.com>
2021-11-26 22:41:37 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,4 @@
-
-    
-      cat <<EOF
+ cat <<EOF
         Please Select:
         1. Log into Lark game
         2. readme
@@ -8,16 +6,13 @@
         0. Quit
 EOF
 read -p "Enter selection [0-3] > " selection
-
      case $selection in
         0)
 break
 ;;
         1) readme=$(<readme.txt)
 echo $readme
-
        ;;
         2) cd .Room #trying to cd into the starting directory but it fails 
 ;;
        esac
-
