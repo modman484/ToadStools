107dbf8538d59faf19b77a7576dc283c42a44556
SamPoly21 <95038313+SamPoly21@users.noreply.github.com>
2021-11-27 11:22:35 -0500

    Update CookBook.sh

--- .hiddenfiles/CookBook.sh
+++ .hiddenfiles/CookBook.sh
@@ -11,23 +11,23 @@ if(gawk ‘$1 ~ /a' )
 read -p "Enter A, B, C, or D (use capitals!): " letter
 case "$letter" in
    A)
-     echo "The cd command is short for 'change direcctory'. This allows you to move yourself between
+     echo "The cd command is short for \"change direcctory\". This allows you to move yourself between
      directories, or in this case, locations."
      ;;
    B)
-     echo "The 'ls'command simply helps list currently available directories and files. In this context, 
-     it's a way to 'look around' and see what kind of locations you can go to, npcs you can interact with, 
+     echo "The \"ls\"command simply helps list currently available directories and files. In this context, 
+     it's a way to \"look around\" and see what kind of locations you can go to, npcs you can interact with, 
      or even check out items!"
      ;;
    C)
-     echo "The 'cat' command looks into the content of files. In this context, this will allow you to interact 
-     with objects, npcs, or even get a description of some things."
+     echo "The \"cat\" command looks into the content of files. In this context, this will allow you to interact 
+     with objects, npcs, or get descriptions of some things."
      ;;
    D) 
-     echo "The '..' command allows you to move up a parent directory. More simply put, it can help you to 
-     'go back' to a location you were in case you get lost."
+     echo "The \"..\" command allows you to move up a parent directory. More simply put, it can help you to 
+     \"go back\" to a location you were in case you get lost."
      ;;
    [a-z] | [E-Z])
-     echo "Whoops, you need to enter one of the correct commands! Try again."
+     echo "Whoops, you need to enter one of the displayed, commands! Try again."
      ;;
 esac     
