9866c7d8fae1b01dc438a729a66624535310a907
modman484 <74793637+modman484@users.noreply.github.com>
2021-11-26 19:27:41 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,22 +1,5 @@
 
-    BG_CYAN="$(tput setab 6)"
-    BG_BLACK="$(tput setab 0)"
-    FG_GREEN="$(tput setaf 2)"
-    FG_MAGENTA="$(tput setaf 5)"
-function resetVar() {
-    echo "Resetting Lark environment for new game, $userName "
-    sleep 2
-    # this is where you could reset variables for new game play
-    }
-
-        tput smcup
-
-    while [[ $REPLY != 0 ]]; do
-      echo -n ${BG_BLACK} ${FG_GREEN}
-      clear
-
-
-# cat uses input of a here document
+    
       cat <<EOF
         Please Select:
         1. Log into Lark game
@@ -25,8 +8,16 @@ function resetVar() {
         0. Quit
 EOF
 read -p "Enter selection [0-3] > " selection
-      # Clear area beneath menu
-      tput cup 10 0 # positions the cursor
-      echo -n ${BG_BLACK}${FG_GREEN}
-      tput ed # Clears curosr to end of line
-      tput cup 11 0
+
+     case $selection in
+        0)
+break
+;;
+        1) readme=$(<readme.txt)
+echo $readme
+
+       ;;
+        2) cd .Room #trying to cd into the starting directory but it fails 
+;;
+       esac
+
