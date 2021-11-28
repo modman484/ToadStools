de70ea01ababe39d06e33f0a37053f9386c1e3be
SamPoly21 <95038313+SamPoly21@users.noreply.github.com>
2021-11-28 15:53:37 -0500

    Update and rename SpaceToad to SpaceToad.sh

---
+++ .Room/Swamp/Spaceship/SpaceToad.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+timeTalk=0 # this number should keep track of how many times we talked to this npc
+echo "Would you look at that, an extraterrestrial toad? Maybe they've got some jokes on hand."
+echo "Or not, comedy's a subjective art."
+
+if [ $timeTalk -ne 1] 
+then
