3c21fd8d47e6d1755c92bcf4844f0df67b7760b0
SamPoly21 <95038313+SamPoly21@users.noreply.github.com>
2021-11-28 22:06:07 -0500

    Update CookBook.sh

--- .hiddenfiles/CookBook.sh
+++ .hiddenfiles/CookBook.sh
@@ -32,9 +32,11 @@ case "$letter" in
      echo "The \"clear\" command is used to clear your screen and reduce visual clutter. It doesn't delete 
      previous lines, so the game remembers what you did. Simply type in this command and hit enter. You can use
      the up arrow key to scroll back up."
+     ;;
    F)
      echo "The '.' command is used to run scripts, but in this context, it'll help you talk to people! Simply type in 
      \". [NPC NAME] [hit enter]\" to talk to them!"
+     ;;
    [a-z] | [G-Z])
      echo "Whoops, you need to enter one of the displayed, commands! Try again."
      ;;
