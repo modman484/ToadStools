b066ca5e435cb25bf576c970449fb619c37ae269
modman484 <74793637+modman484@users.noreply.github.com>
2021-12-08 21:55:28 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 # cat uses input of a here document
-cd ../../../
+cd Mountain/../../ 2> /dev/null
 
       cat <<EOF
         Please Select:
@@ -22,49 +22,50 @@ echo $readme
         TS=0
 
        echo "100" > .Vars/Health
-       echo 0 > .Vars/TS
-       echo 0 > .Vars/Var1
-       echo 0 > .Vars/Var2
+       echo "0" > .Vars/TS
+       echo "0" > .Vars/Var1
+       echo "0" > .Vars/Var2
        echo 100 > .Vars/EHP
         export PHP=100
         export TS=0
         export c1=0
         export c2=0
         export jk=0
-        mv .Room/Swamp/Hut .hiddenfiles
+        mv .Room/Swamp/Hut .hiddenfiles 2> /dev/null
         cd .Room
  #trying to cd into the starting directory but it fails
 cat <<ART
                      " /oo/.``---`     `:/:
-                     .yo++osooooo---::sssss-
-                    :+o++++o+yy+so+++++++so++/
+                     .yo++osooooo---::sssss-\`
+                    :+o++++o+yy+so+++++++so++/\`
                    :s+s++++++oo+++++++osyo++oso:
                    /yo+++++++++++oosoo+so+++++s-
-                     :/::::::///+++yo+++y+++++++y
+                     :/::::::///+++yo+++y+++++++y\`
                      ++oos/:::--::/s++oos+++++++oo..
                      +++s+:::://::+oo+:/y+++++sooooo/
                      -+/+/:::++ossosyso+ho++++++++++o
-                     /:::///ooo++oyso+//soooooos++o-
-                       o/+//:--:::so::--::++o:s+++.
-                       +-/  `..o//y:-....`+ooo+:.
+                     \`/:::///ooo++oyso+//soooooos++o-
+                       \`o/+//:--:::so::--::++o:s+++.
+                       \`+-/  \`..o//y:-....\`+ooo+:.
                        /-:/     +-/o/     +s++o
-                       +-+`     +-oso::--.`.+/s-
-                      .oo+:--:///-sss:://+o+s-s`
+                       +-+\`     +-oso::--.\`.+/s-
+                      .oo+:--:///-sss:://+o+s-s\`
                       /-+so+//:s--hso/-:::/+/+/.
-                      +-+`   `-o-:o///::----:/-/
-                     .+-+      +-+-o+o.`     +-+
+                      +-+\`   \`-o-:o///::----:/-/
+                     .+-+      +-+-o+o.\`     +-+
                      /:/:      +-o oso++//:--o-+
                      oo//:-.--//-o/o+o--:///+y-/.
-                    .+:s+++//+y--s/o++:   `.-//-/
-                    +-:/-..`.:o-:+-/+++::::::/+/+
-                    o-+       +-+/::++o----..`+-o
-                   ./-/      -/-+`` o/s       /-+.
+                    .+:s+++//+y--s/o++:   \`.-//-/
+                    +-:/-..\`.:o-:+-/+++::::::/+/+
+                    o-+       +-+/::++o----..\`+-o
+                   ./-/      -/-+\`\` o/s       /-+.
                    /-:-      /--+   .--       ./:/
-                   o-+`      +:-/              o-+
-                   ///      `+/+-              ::-
-                            -//o`
+                   o-+\`      +:-/              o-+
+                   ///      \`+/+-              ::-
+                            -//o\`
                             .://  "
 ART
+
 echo "you wake up in a room with a pice of paper and a book stand"
 echo "\$ls can be used to look around"
 ;;
